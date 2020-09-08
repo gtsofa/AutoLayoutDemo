8279a4cacf41bd74e6c0e8989c43b7ddb2fde634
gtsofa <jlsnyule@yahoo.com>
2020-09-08 12:42:03 +0300

    Initial Commit

---
+++ AutoLayoutDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutDemo
+//
+//  Created by Julius on 08/09/2020.
+//  Copyright © 2020 Julius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
